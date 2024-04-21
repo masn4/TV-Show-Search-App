7c2b96f5aa8edb0992f47874eb80a186db64d2f1
masn4 <134996256+masn4@users.noreply.github.com>
2024-04-21 15:29:18 -0500

    Added search functionality

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
-  console.log("SUBMITTED");
+  const searchInput = form.elements.query.value;
+  const result = await axios.get(
+    `http://api.tvmaze.com/search/shows?q=${searchInput}`
+  );
+  console.log(result.data);
+  result.data.forEach((q) => {
+    try {
+      const img = document.createElement("IMG");
+      img.src = q.show.image.medium;
+      document.body.append(img);
+    } catch (e) {
+      console.log("NO IMAGE :(");
+      return "NO IMAGE";
+    }
+  });
+  // const img = document.createElement("IMG");
+  // img.src = result.data[0].show.image.medium;
+  // document.body.append(img);
 });
